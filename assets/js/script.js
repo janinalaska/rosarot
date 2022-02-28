3ebce1f35585cf797b82fd866ada7dfcc981e8df
alaskamay <37851909+alaskamay@users.noreply.github.com>
2022-02-28 15:28:25 +0100

    activated colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,14 +45,14 @@ function changeColor() {
   var randomColor = Math.floor(Math.random() * bgColor.length);
   // console.log(randomColor);
   // svgElement.style.fill = color[randomColor];
-  // r.style.setProperty('--bgColor', bgColor[randomColor]);
-  // r.style.setProperty('--bgColA', bgColor[randomColor]+'01');
-  // r.style.setProperty('--color', color[randomColor]);
+  r.style.setProperty('--bgColor', bgColor[randomColor]);
+  r.style.setProperty('--bgColA', bgColor[randomColor]+'01');
+  r.style.setProperty('--color', color[randomColor]);
 
   // svgElement.style.fill = color[s];
-  r.style.setProperty('--bgColor', bgColor[s]);
-  r.style.setProperty('--bgColA', bgColor[s]+'01');
-  r.style.setProperty('--color', color[s]);
+  // r.style.setProperty('--bgColor', bgColor[s]);
+  // r.style.setProperty('--bgColA', bgColor[s]+'01');
+  // r.style.setProperty('--color', color[s]);
 };
 
 changeColor(); // do this in window.onload !!  window.onload(changeColor)
